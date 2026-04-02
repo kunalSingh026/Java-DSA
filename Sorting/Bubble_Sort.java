4409f452656b23ec7236189e48ff90219abb8e8b
kunalSingh026 <skunalkumar759@gmail.com>
2026-04-02 19:59:17 +0530

    Bubble Sort Code uploaded

---
+++ Sorting/Bubble_Sort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        
+        bubbleSort(arr);
+        
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) break;
+        }
+    }
+}
